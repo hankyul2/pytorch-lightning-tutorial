3f7217b709aced928e5a52b44213b64418fe59e2
hankyul <consistant1y@ajou.ac.kr>
2021-09-21 11:35:09 +0900

    feat: Apply LightningDataModule
    - Apply LightningDataModule

--- src/getting_started/lightning_in_2_steps/basic_mnist.py
+++ src/getting_started/lightning_in_2_steps/basic_mnist.py
@@ -1,7 +1,10 @@
+from typing import Optional
+
 import torch
+from pytorch_lightning.utilities.types import TRAIN_DATALOADERS, EVAL_DATALOADERS
 from torch import nn
 from torch import optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torchvision import transforms, datasets
 import torch.nn.functional as F
 import pytorch_lightning as pl
@@ -20,14 +23,40 @@ class BaseImageClassificationSystem(pl.LightningModule):
         x, y = batch
         y_hat = self.fc(torch.flatten(self.backbone(x), 1))
         loss = F.cross_entropy(y_hat, y)
-        self.log('train/loss', loss)
         return loss
 
     def configure_optimizers(self):
         return optim.SGD(self.parameters(), lr=0.01)
 
-train_dl = DataLoader(datasets.MNIST(root='./', train=True, transform=transforms.ToTensor(), download=True),
-                      batch_size=128)
+
+class MNISTDataModule(pl.LightningDataModule):
+    def __init__(self, batch_size):
+        super(MNISTDataModule, self).__init__()
+        self.batch_size = batch_size
+
+    def prepare_data(self) -> None:
+        datasets.MNIST(root='./', train=True, transform=transforms.ToTensor(), download=True)
+        datasets.MNIST(root='./', train=False, transform=transforms.ToTensor(), download=True)
+
+    def setup(self, stage: Optional[str] = None):
+        if stage in (None, 'fit'):
+            mnist_train = datasets.MNIST(root='./', train=True, transform=transforms.ToTensor(), download=True)
+            self.train_ds, self.valid_ds = random_split(mnist_train, [55000, 5000])
+        elif stage in (None, 'test'):
+            mnist_test = datasets.MNIST(root='./', train=False, transform=transforms.ToTensor(), download=True)
+            self.test_ds = mnist_test
+
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
+        return DataLoader(self.train_ds, batch_size=self.batch_size)
+
+    def val_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.valid_ds, batch_size=self.batch_size)
+
+    def test_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.test_ds, batch_size=self.batch_size)
+
+
+dl = MNISTDataModule(batch_size=128)
 model = BaseImageClassificationSystem()
-trainer = pl.Trainer(gpus=1)
-trainer.fit(model, train_dl)
+trainer = pl.Trainer(gpus=8, accelerator='ddp', max_epochs=100, profiler="simple")
+trainer.fit(model, dl)
